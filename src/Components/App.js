8d26cb1bb2d69f2999f2efb593a5ba29cb2c2243
mayanksingh-tech <kingrocco67@gmail.com>
2020-09-09 05:16:52 +0530

    final commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar'
 import Youtube from '../apis/Youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends Component {
 
+    componentDidMount() {
+        this.onTermSubmit('Indian army')
+    }
+
+    
+
     state= {
-        videos : []
+        videos : [],
+        selectedVideo: null
     }
 
     onTermSubmit = async term => {
@@ -15,14 +23,32 @@ class App extends Component {
                 q : term
             }
         });
-        this.setState({videos: response.data.items})   
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })   
+     }
+
+     onVideoSelect = (video) => {
+         this.setState({ selectedVideo: video })
      }
 
     render() {
         return (
             <div className='ui container'>
               <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+            
+                <div className='ui grid'>
+                    <div className='ui row'>
+                        <div className='eleven wide column'>
+                            <VideoDetail video={this.state.selectedVideo} />
+                            </div>
+                        <div className='five wide column'>
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                            </div>
+                    </div>
+                </div>
+
             </div>
         );
     }
